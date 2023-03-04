1adf40d61f70f5da02331ae550b71ffdacb1b23c
lichen <1176343828@qq.com>
2023-03-04 18:19:14 +0800

    Create two_dimensional test

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int arr[2][3] = {{1, 2, 3}, {4, 5, 6}};
+
+
+    int *a = (arr + 1);
+    printf("%p, %p\n", a, &arr[1][0]);
+
+    printf("%p, %p\n", &arr, &arr[0][0]);
+
+    //printf("%p", &arr, &(arr + 1));
+    printf("%p", &a);
+
+
     return 0;
 }
