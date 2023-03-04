c792039163a5390ddab1fa9d14e751ba3fcdaadd
lichen <1176343828@qq.com>
2023-03-04 17:49:21 +0800

    Create hello world program

---
+++ cmake-build-debug/CMakeFiles/Windows_C_Study.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Windows_C_Study.dir/main.c.obj"
+  "Windows_C_Study.exe"
+  "Windows_C_Study.exe.manifest"
+  "Windows_C_Study.pdb"
+  "libWindows_C_Study.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Windows_C_Study.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
